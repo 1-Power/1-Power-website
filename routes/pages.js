02c56632cdb9fe666b0a78aac4bd431dbdc18754
rafay99-epic <99marafay@gmail.com>
2022-10-09 02:52:59 +0500

    Final Version

--- routes/pages.js
+++ routes/pages.js
@@ -4,13 +4,13 @@ const express = require("express");
 const router = express.Router();
 
 
-//this will render the loading screen page
-router.get("/", (req, res) => {
-    res.render("./pages/loading");
+// //this will render the loading screen page
+// router.get("/", (req, res) => {
+//     res.render("./pages/loading");
     
-});
+// });
 //loading the index page
-router.get("/index", (req, res) => {
+router.get("/", (req, res) => {
     res.render("./pages/index");
 });
 
